b4cff986d458bb2271285adc8859ba7204cdb744
DESKTOP-UKMJSA7\game <000000>
2026-05-21 17:03:40 +0900

    뭐지? 뭐했지?

--- Assets/Scripts/Objects/Card/CardResolver.cs
+++ Assets/Scripts/Objects/Card/CardResolver.cs
@@ -2,53 +2,53 @@ using UnityEngine;
 
 public class CardResolver
 {
-    //public bool Use(CardData card, CharacterBase user, CharacterBase target)
-    //{
-    //    Debug.Log("카드 사용 시도");
-    //
-    //    if (!CanUse(card, user))
-    //    {
-    //        Debug.Log("코스트 부족 또는 CostModule 없음");
-    //        return false;
-    //    }
-    //
-    //    Debug.Log("코스트 통과");
-    //
-    //    CostModule costModule = user.GetModule<CostModule>();
-    //
-    //    foreach (CardCostData cost in card.costs)
-    //    {
-    //        costModule.Use(cost.costType, cost.amount);
-    //    }
-    //
-    //    if (HasTag(card, CardTagType.Attack))
-    //    {
-    //        Debug.Log("공격 카드 확인");
-    //
-    //        DamageStruct damageInfo = new DamageStruct
-    //        {
-    //            from = user.gameObject,
-    //            instigator = user,
-    //            damageAmount = card.damage,
-    //            critical = false,
-    //            damageType = DamageType.Physical
-    //        };
-    //
-    //        CombatModule targetCombat = target.GetModule<CombatModule>();
-    //
-    //        if (targetCombat == null)
-    //        {
-    //            Debug.LogError("대상 CombatModule 없음");
-    //            return false;
-    //        }
-    //
-    //        Debug.Log($"대미지 적용 시도: {damageInfo.damageAmount}");
-    //
-    //        targetCombat.OnHit(damageInfo);
-    //    }
-    //
-    //    return true;
-    //}
+   public bool Use(CardData card, ControllerBase user, ControllerBase target)
+   {
+       Debug.Log("카드 사용 시도");
+   
+       if (!CanUse(card, user))
+       {
+           Debug.Log("코스트 부족 또는 CostModule 없음");
+           return false;
+       }
+   
+       Debug.Log("코스트 통과");
+   
+       CostModule costModule = user.GetModule<CostModule>();
+   
+       foreach (CardCostData cost in card.costs)
+       {
+           costModule.Use(cost.costType, cost.amount);
+       }
+   
+       if (HasTag(card, CardTagType.Attack))
+       {
+           Debug.Log("공격 카드 확인");
+   
+           DamageStruct damageInfo = new DamageStruct
+           {
+               from = user.gameObject,
+               instigator = user,
+               damageAmount = card.damage,
+               critical = false,
+               damageType = DamageType.Physical
+           };
+   
+           CombatModule targetCombat = target.GetModule<CombatModule>();
+   
+           if (targetCombat == null)
+           {
+               Debug.LogError("대상 CombatModule 없음");
+               return false;
+           }
+   
+           Debug.Log($"대미지 적용 시도: {damageInfo.damageAmount}");
+   
+           targetCombat.OnHit(damageInfo);
+       }
+   
+       return true;
+   }
     
     
     public bool CanUse(CardData card, ControllerBase user)
@@ -67,48 +67,6 @@ public class CardResolver
         return true;
     }
 
-    public bool Use(CardData card, ControllerBase user, ControllerBase target)
-    {
-        if (!CanUse(card, user))
-            return false;
-
-        CostModule costModule = user.GetModule<CostModule>();
-
-        foreach (CardCostData cost in card.costs)
-        {
-            costModule.Use(cost.costType, cost.amount);
-        }
-
-        if (HasTag(card, CardTagType.Attack))
-        {
-            DamageStruct damageInfo = new DamageStruct
-            {
-                from = user.gameObject,
-                instigator = user,
-                damageAmount = card.damage,
-                critical = false,
-                damageType = DamageType.Physical
-            };
-
-            CombatModule targetCombat = target.GetModule<CombatModule>();
-            targetCombat.OnHit(damageInfo);
-        }
-
-        if (HasTag(card, CardTagType.Restore))
-        {
-            RestoreStruct restoreInfo = new RestoreStruct
-            {
-                from = user.gameObject,
-                instigator = user,
-                restoreAmount = card.restoreAmount
-            };
-
-            HitpointModules targetHp = target.GetModule<HitpointModules>();
-            targetHp.TakeRestore(restoreInfo);
-        }
-
-        return true;
-    }
 
     private bool HasTag(CardData card, CardTagType tag)
     {
---
+++ Assets/Scripts/Objects/Characters/CharacterModules/Experiment/EX_Characters/CardClick.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CardCrkClick : MonoBehaviour
+{
+    
+    [SerializeField] private CharacterBase user;
+    [SerializeField] private CharacterBase target;
+
+    private void OnEnable()
+    {
+        InputManager.OnMouseLeftButton -= OnClick;
+        InputManager.OnMouseLeftButton += OnClick;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.OnMouseLeftButton -= OnClick;
+    }
+
+    private void OnClick(bool value, Vector2 screenPosition, Vector3 worldPosition)
+    {
+        if (!value)
+            return;
+
+        GameObject clickedObject =
+            GameManager.Instance.Input.GetGameObjectUnderCursor();
+
+        UI_Card card =
+            clickedObject?.GetComponentInParent<UI_Card>();
+
+        if (card == null)
+            return;
+
+        Debug.Log($"카드 클릭: {card.CardData.cardName}");
+
+        CardResolver resolver = new CardResolver();
+
+        resolver.Use(card.CardData, user.Controller, target.Controller);
+    }
+}
+
